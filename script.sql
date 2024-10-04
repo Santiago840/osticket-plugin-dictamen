7db7affb248a24ce5d6eeb0416ad034ce705df38
Santiago840 <santyemma32@gmail.com>
2024-10-04 13:49:05 -0600

    agregación de  archivos de dictaminación y scriptsql
    - Falta probarlo en ambiente de desarrollo

---
+++ script.sql
@@ -0,0 +1,27 @@
+use osticket;
+
+CREATE TABLE
+    IF NOT EXISTS ost_dictaminacion (
+        id_dictaminacion INT AUTO_INCREMENT PRIMARY KEY,
+        id_staff INT NOT NULL,
+        id_ticket INT NOT NULL,
+        id_estado INT NOT NULL,
+        id_valoracion INT NOT NULL
+    ) ENGINE = InnoDB;
+
+CREATE TABLE
+    IF NOT EXISTS ost_dictaminacion_asignaciones (
+        id_Asignacion INT AUTO_INCREMENT PRIMARY KEY,
+        id_ticket INT NOT NULL,
+        id_staff INT NOT NULL
+    ) ENGINE = InnoDB;
+
+CREATE TABLE
+    IF NOT EXISTS ost_dictaminacion_respuestas (
+        id_respuesta INT AUTO_INCREMENT PRIMARY KEY,
+        id_staff INT NOT NULL,
+        id_ticket INT NOT NULL,
+        pregunta TEXT,
+        pregunta_label text,
+        respuesta TEXT
+    ) ENGINE = InnoDB;
